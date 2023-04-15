ad661e5aa35f2fbf585bdd1a0e66abffe436aad8
anasmoundir <anasmoundir1996@gmail.com>
2023-04-15 21:28:46 +0000

    two migration

---
+++ database/migrations/2023_04_15_212132_create_course_progress_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_progress', function (Blueprint $table) {
+          
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('lesson_id');
+            $table->boolean('completed')->default(false);
+            $table->boolean('is_current')->default(false);
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_progress');
+    }
+};
